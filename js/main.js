2148ff600bdb147d97c1387b27ec66e1b375c40f
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-04-18 17:10:02 +0200

    setting delete btn

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let success = document.getElementById("success");
 let fail = document.getElementById("fail");
 let list = document.getElementById("list");
 let show = document.getElementById("show");
+
 // * Adding Tasks
 btn.addEventListener("click", function () {
   if (txt.value == false) {
@@ -22,25 +23,33 @@ btn.addEventListener("click", function () {
 });
 
 //* Showing Tasks
-show.addEventListener("click", function () {
-  // tasks.forEach((task) => {
-  let li = document.createElement("li");
-  let text = document.createElement("span");
-  let del = document.createElement("button");
-  li.classList.add(
-    "list-group-item",
-    "d-flex",
-    "justify-content-between",
-    "align-items-center"
-  );
-  del.classList.add("btn", "btn-danger");
-  del.textContent = "Delete";
+// if (tasks.length > 0) {
+//   tasks.forEach((task) => {
+let li = document.createElement("li");
+let text = document.createElement("span");
+let del = document.createElement("button");
+li.classList.add(
+  "list-group-item",
+  "d-flex",
+  "justify-content-between",
+  "align-items-center"
+);
+text.textContent = txt.value;
+del.classList.add("btn", "btn-danger");
+del.textContent = "Delete";
 
-  li.appendChild(text);
-  li.appendChild(del);
+li.appendChild(text);
+li.appendChild(del);
 
-  document.getElementById("list").appendChild(li);
+document.getElementById("list").appendChild(li);
+//   });
+// } //end if
+
+//! Delete btn
+del.addEventListener("click", function () {
+  let parentDev = this.parentNode;
+  parentDev.remove();
 });
-// });
+
 //*-----------------------------
 console.log(tasks);
