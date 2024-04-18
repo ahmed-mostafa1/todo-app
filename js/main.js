f691e9895cf752e66c7e448b04d8696363f9d8ef
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-04-18 16:00:09 +0200

    added basic events

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 // ! Functions Area
 
 // ! Set Elements Area
+let txt = document.getElementById("input");
 let btn = document.getElementById("add");
 
 // ! Events Area
+btn.addEventListener("click", function () {
+  if (txt.value == false) {
+    alert("Please Enter a task!");
+  } else {
+    console.log(txt.value);
+    txt.value = "";
+  }
+});
