2b43ba31b8081666cf2d72d41269d4107b8ef247
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-04-18 16:50:22 +0200

    setting Show tasks

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-// ! Diclarations Area
+// * Diclarations
 let tasks = [];
 
-// ! Functions Area
-
-// ! Set Elements Area
+// * Set Elements
 let txt = document.getElementById("input");
 let btn = document.getElementById("add");
 let success = document.getElementById("success");
 let fail = document.getElementById("fail");
-
-// ! Events Area
+let list = document.getElementById("list");
+let show = document.getElementById("show");
+// * Adding Tasks
 btn.addEventListener("click", function () {
   if (txt.value == false) {
     success.style.display = "none";
@@ -18,8 +17,30 @@ btn.addEventListener("click", function () {
     tasks.push(txt.value);
     success.style.display = "block";
     fail.style.display = "none";
-
     txt.value = "";
   }
 });
+
+//* Showing Tasks
+show.addEventListener("click", function () {
+  // tasks.forEach((task) => {
+  let li = document.createElement("li");
+  let text = document.createElement("span");
+  let del = document.createElement("button");
+  li.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+  del.classList.add("btn", "btn-danger");
+  del.textContent = "Delete";
+
+  li.appendChild(text);
+  li.appendChild(del);
+
+  document.getElementById("list").appendChild(li);
+});
+// });
+//*-----------------------------
 console.log(tasks);
