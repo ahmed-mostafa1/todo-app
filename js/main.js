1d71b1d3fc2a9387dee667e06d6905a75cd12f33
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-04-18 16:16:59 +0200

    setting up allerts

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
+// ! Diclarations Area
+let tasks = [];
+
 // ! Functions Area
 
 // ! Set Elements Area
 let txt = document.getElementById("input");
 let btn = document.getElementById("add");
+let success = document.getElementById("success");
+let fail = document.getElementById("fail");
 
 // ! Events Area
 btn.addEventListener("click", function () {
   if (txt.value == false) {
-    alert("Please Enter a task!");
+    success.style.display = "none";
+    fail.style.display = "block";
   } else {
-    console.log(txt.value);
+    tasks.push(txt.value);
+    success.style.display = "block";
+    fail.style.display = "none";
+
     txt.value = "";
   }
 });
+console.log(tasks);
