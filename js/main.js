59a96e5dccf8aac4e7af42f52057bd70beae2055
Ahmed Mostafa <sitesdesigner96@gmail.com>
2024-04-18 21:50:32 +0200

    Storing in localStorage

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ let list = document.getElementById("list");
 let show = document.getElementById("show");
 
 // * Adding Tasks
+txt.focus();
 btn.addEventListener("click", function () {
   if (txt.value == false) {
     success.style.display = "none";
@@ -18,38 +19,40 @@ btn.addEventListener("click", function () {
     tasks.push(txt.value);
     success.style.display = "block";
     fail.style.display = "none";
+    // txt.value = "";
+    // }
+    // });
+
+    //* Showing Tasks
+    // if (tasks.length > 0) {
+    //   tasks.forEach((task) => {
+    let li = document.createElement("li");
+    let text = document.createElement("span");
+    let del = document.createElement("button");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "align-items-center"
+    );
+    text.textContent = txt.value;
     txt.value = "";
+    del.classList.add("btn", "btn-danger");
+    del.textContent = "Delete";
+
+    li.appendChild(text);
+    li.appendChild(del);
+
+    document.getElementById("list").appendChild(li);
+    //   });
+    // } //end if
+
+    //! Delete btn
+    del.addEventListener("click", function () {
+      let parentDev = this.parentNode;
+      parentDev.remove();
+    });
   }
-});
-
-//* Showing Tasks
-// if (tasks.length > 0) {
-//   tasks.forEach((task) => {
-let li = document.createElement("li");
-let text = document.createElement("span");
-let del = document.createElement("button");
-li.classList.add(
-  "list-group-item",
-  "d-flex",
-  "justify-content-between",
-  "align-items-center"
-);
-text.textContent = txt.value;
-del.classList.add("btn", "btn-danger");
-del.textContent = "Delete";
-
-li.appendChild(text);
-li.appendChild(del);
-
-document.getElementById("list").appendChild(li);
-//   });
-// } //end if
-
-//! Delete btn
-del.addEventListener("click", function () {
-  let parentDev = this.parentNode;
-  parentDev.remove();
-});
-
+}); // end of add task btn
 //*-----------------------------
 console.log(tasks);
